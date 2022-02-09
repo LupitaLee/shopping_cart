3a1f194d92f2a33ccaa688aceb9be3fa4d5469a8
Lupita <lupitarivera8899@gmail.com>
2022-02-09 16:21:42 -0600

    add a navar using react boostrap

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Container, Navbar } from 'react-bootstrap'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+        <Navbar bg="dark" variant="dark" styler={{height:80}}>
+            <Container>
+                <Navbar.Brand>
+                    <a href='/'>  Shooping cart</a>
+                </Navbar.Brand>
+            </Container>
+        </Navbar>
+    </div>
   )
 }
 
