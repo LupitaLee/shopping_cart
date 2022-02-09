78f8cfeae44ad1893ea839ccc2f3b60b9cae81a6
Lupita <lupitarivera8899@gmail.com>
2022-02-09 16:29:37 -0600

    style navbar adding a searchbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
-import { Container, Navbar } from 'react-bootstrap'
+import { Container, FormControl, Navbar } from 'react-bootstrap'
 
 const Header = () => {
   return (
     <div>
-        <Navbar bg="dark" variant="dark" styler={{height:80}}>
+        <Navbar bg="dark" variant="dark" style={{height: 80}}>
             <Container>
                 <Navbar.Brand>
                     <a href='/'>  Shooping cart</a>
+                    <Navbar.Text className="search">
+                        <FormControl
+                        style={{width: 500}}
+                         placeholder="Search a Product"
+                         className='m-auto'
+                        
+                        />
+                           
+                     
+                    </Navbar.Text>
                 </Navbar.Brand>
             </Container>
         </Navbar>
